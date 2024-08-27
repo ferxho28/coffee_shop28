0a47b4ff025994e4b1a4b09d101c5fc8de6e8893
Fernando Alfonso <ferchomill@outlook.es>
2024-08-27 11:42:12 -0500

    feat(products)(coffe_shop): Update and files models,views,urls and forms for model product.

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o_l_7nre)5ksip)8m$koui)wp(zck%rept&x0+b-u7%@s2+d$d'
+SECRET_KEY = 'django-insecure--+916&ukvq2-f-@^418i1%)&-mtb7)nmo2xw$-1oi$yu^4ubi)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'coffee_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
