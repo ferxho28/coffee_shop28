b281e58b761c803f2c58643cfe73468235cca993
Fernando Alfonso <ferchomill@outlook.es>
2024-08-29 11:50:34 -0500

    Feat(users): add user authentication with templates and URL routing
    - Create ferxho ferxho app with  and  templates.
    - Design forms in the templates using Tailwind CSS and Crispy Forms.
    - Set up URL routing for ferxho ferxho app, including connections to global .

--- products/models.py
+++ products/models.py
@@ -9,6 +9,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="precio")
     available = models.BooleanField(default=True,verbose_name="disponible")
     photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="foto")
+    
 
     def __str__(self) :
         return self.name
